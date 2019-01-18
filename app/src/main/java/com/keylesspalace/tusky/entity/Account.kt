d90a0b729bcab004e0e10e630e0f62fcd726afd5
kyori19 <kyori.dis@gmail.com>
2019-01-18 21:02:04 +0900

    notestockのアカウントIDを取得できるように改善

--- app/src/main/java/com/keylesspalace/tusky/entity/Account.kt
+++ app/src/main/java/com/keylesspalace/tusky/entity/Account.kt
@@ -30,7 +30,7 @@ import java.util.*
 data class Account(
         val id: String,
         @SerializedName("username") val localUsername: String,
-        @SerializedName("acct") val username: String,
+        @SerializedName("acct", alternate = ["subject"]) val username: String,
         @SerializedName("display_name") val displayName: String,
         val note: @WriteWith<SpannedParceler>() Spanned,
         val url: String,
